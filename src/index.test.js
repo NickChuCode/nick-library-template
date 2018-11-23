e94d5342ab6f2c3571820f7de040c6fa4ffe2629
NickChuCode <nickchu15@outlook.com>
2018-11-23 23:39:25 +0800

    add mocha chai testing and write a suite of test

---
+++ src/index.test.js
@@ -0,0 +1,33 @@
+// node原生的测试方法，不用外部库也可以做测试，但测试库可以做的更多，比如watch变化，自动进行一系列的测试等
+// const assert = require("assert")
+//
+// assert(1 === 2, 'one is not two..')
+
+// 下面是mocha + chai的测试
+// 把测试文件和被测试的源代码放在一个文件夹下是kent推荐的一种方式，因为当源文件很多的时候，
+// 如果放在test文件夹下，我们需要分别定位src文件和test文件，还不如放在一起，好维护
+'use strict'
+
+var expect = require('chai').expect
+var should = require('chai').should()
+// 在node中，可以用.代替./index
+const starwarsNames = require('.')
+
+describe('starwars-names', function () {
+    it('should have a list a available names', function () {
+        expect(isArrayOfStrings(starwarsNames.all)).to.be.true
+    });
+    it('should allow me to get a random name from the list', function () {
+        expect(starwarsNames.random()).to.satisfy(isIncludedIn(starwarsNames.all))
+    });
+})
+
+function isArrayOfStrings(arr) {
+    return arr.every((item) => typeof item === 'string')
+}
+
+function isIncludedIn(array) {
+    return function (item) {
+        return starwarsNames.all.indexOf(item) > -1
+    }
+}
